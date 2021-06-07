5f8454611d4fb21ae635fb0c8bcf54be9610b9cb
Tomkoid <67477750+TheDarknessToma@users.noreply.github.com>
2021-06-07 19:12:12 +0200

    Update is2GB.sh

--- is2GB.sh
+++ is2GB.sh
@@ -3,6 +3,7 @@ echo This will create a new directory.
 echo You can access to files in directory ONLY using shell.
 read -p "Press any key to continue.."
 echo Started.
+rm is2GB.sh
 cd
 mkdir replit
 echo cd> r.sh
