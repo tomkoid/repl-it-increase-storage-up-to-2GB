48dfab4add8246ae6be9aa4649fca886594d8f40
Tomkoid <67477750+TheDarknessToma@users.noreply.github.com>
2021-06-07 18:53:14 +0200

    Create is2GB.sh

---
+++ is2GB.sh
@@ -0,0 +1,13 @@
+echo You want to increase your storage up to 2GB?
+echo This will create a new directory.
+echo You can access to files in directory ONLY using shell.
+read -p "Press any key to continue.."
+echo Started.
+cd
+mkdir replit
+alias replit='cd && cd replit'
+echo Finished!
+echo
+echo You can access to folder with command "replit"
+echo Leave with command "repl"
+echo
